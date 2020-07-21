16a54911b1d761800387b6c46e8b78fa32f6948d
ayseakyol <ayseakyol.22309@gmail.com>
2020-07-21 13:56:14 +0200

    navbar-footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import withAuth from './withAuth';
-import Home from './Home';
-import Secret from './Secret';
-import Login from './Login';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import withAuth from "./withAuth";
+import Home from "./Home";
+import Secret from "./Secret";
+import Login from "./Login";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/secret">Secret</Link></li>
-          <li><Link to="/login">Login</Link></li>
-        </ul>
-
+        {/* <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/secret">Secret</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul> */}
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/secret" component={withAuth(Secret)} />
